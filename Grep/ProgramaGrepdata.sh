ff183b5e302ea29b45f875f21ed48f2dcc9444f4
unknown <luis.silva@VISUALSALE2.mind.com.co>
2013-03-08 10:23:05 -0500

    Taller2

---
+++ Grep/ProgramaGrepdata.sh
@@ -0,0 +1,12 @@
+grep -i "\<x" grepdata.txt
+grep -i "^[0-9]\{3\}\( \)"
+grep -i "\( \)\2<[0-9]\{3\}"
+grep -i "\([aeiou]\)[...]\([aeiou]\)" grepdata.txt
+grep -i "[^-Sa-z]" grepdata.txt
+grep -i "\( \)\(CA\)\( \)" grepdata.txt
+grep -i "\@" grepdata.txt
+grep -i "[^\(sep.\)\( \)]" grepdata.txt
+grep -i "\( \)\(de\)\( \)" grepdata.txt
+
+ 
+ 
